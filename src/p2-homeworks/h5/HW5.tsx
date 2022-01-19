5a93574d06e961fa827c7528ef539f02c00cde89
Kirill Pravdin <kirill-pravdin@mail.ru>
2022-01-19 21:56:23 +0300

    HashRouter

--- src/p2-homeworks/h5/HW5.tsx
+++ src/p2-homeworks/h5/HW5.tsx
@@ -1,16 +1,12 @@
 import React from 'react'
 import {Header} from './Header'
 import {RoutesApp} from './Routes'
-import {HashRouter} from "react-router-dom";
 
 function HW5() {
     return (
         <div>
-            <HashRouter>
-                {/*в gh-pages лучше работает HashRouter*/}
                 <Header/>
                 <RoutesApp/>
-            </HashRouter>
         </div>
     )
 }
